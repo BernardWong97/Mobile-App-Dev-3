94210ddec432f4fcab51dd413a19c0e27d8da3c9
Bernard Wong <ben.wc88@gmail.com>
2019-12-09 14:56:09 +0000

    Implement player death when fall

---
+++ Grapple Ball/Assets/_Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -10)
+            Die();
+    }
+
+    void Die()
+    {
+        Debug.Log("Player is dead");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
