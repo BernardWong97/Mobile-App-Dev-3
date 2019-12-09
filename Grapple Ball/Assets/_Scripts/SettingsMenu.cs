525011a83a96ebed1408ef89df7e1770b0d93439
Bernard Wong <ben.wc88@gmail.com>
2019-12-09 03:54:01 +0000

    Added main menu music and scripted music slider with mixer

---
+++ Grapple Ball/Assets/_Scripts/SettingsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void SetMusic(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+}
